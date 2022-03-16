23e99689f2da8762ea1e89b1cc3a73b8f2df3cad
Penfold <Harry.Penfold@archant.co.uk>
2022-03-16 09:48:57 +0000

    two way binding example

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+
 /* React reads through each function until everything is executed, which means that we need 'state' to reevalute
 a function after everything is initially executed to change something in the UI. */
 
@@ -7,23 +7,14 @@ import Card from '../UI/Card';
 import '../components_css/ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  
-  const [title, setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluated by React');
-  
-  const clickHandler = () => {
-    setTitle('Updated!');
-    console.log(title);
-  };
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>£{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
