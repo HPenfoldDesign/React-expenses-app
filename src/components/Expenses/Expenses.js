23413cf9f765349cab83fa0da6cec0ebc3143ba8
Penfold <Harry.Penfold@archant.co.uk>
2022-03-28 17:23:17 +0100

    added conditional to display data dependant on year selected

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,25 +13,34 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
+
+  let expensesContent = <p>No Expenses found.</p>;
+  if(filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          /*key stops react generating errors when producing the list of expenses on the page, by generating a unique id from the
+          'saveExpenseDataHandler' function for each expense, It takes the thinking time away from the browser which essentially improves performance and prevents bugs.*/
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date} />
+      );
+    })
+  }
+
   return (
     <div>
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      
-      {props.items.map((expense) => ( 
-        <ExpenseItem 
-        key={expense.id} 
-        /*key stops react generating errors when producing the list of expenses on the page, by generating a unique id from the 
-        'saveExpenseDataHandler' function for each expense, It takes the thinking time away from the browser which essentially improves performance and prevents bugs.*/ 
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-        />
-      ))}
-      
+      {expensesContent}
     </Card>
     </div>
   );
 }
 
 export default Expenses;
+
